da7969f7f6058e6edb1c5076f2a26d75b903f501
yumster <lina.vasileva93@gmail.com>
2017-05-23 15:09:13 +0300

    game done. need bug fix && tests

--- game.js
+++ game.js
@@ -1,28 +1,66 @@
 //todo use  block scoping (let)
-//todo use spread / rest operator
+//todo use spread / rest operator    rest done
 //todo use default settings
 //todo use string interpolation
 //todo use arrow functions
-//todo use classes + inheritance + super
+//todo use classes + inheritance + super    done
 
 
-class Fighter{
+class Fighter {
     constructor(name, power, health) {
         this.name = name;
         this.power = power;
         this.health = health;
     }
 
-    setDamage(damage){
+    setDamage(damage) {
         this.health -= damage;
     }
 
-    hit(enemy, point){
+    hit(enemy, point) {
+        let damage = point * this.power;
         enemy.setDamage(damage);
+        return enemy.health;
     }
 };
 
-
-function fight(fighter, improvedFighter, point){
-
+class ImprovedFighter extends Fighter {
+    hit(enemy, point) {
+        let newPoint = point * 2;
+        super.hit(enemy, newPoint);
+    }
 }
+
+function fight(fighter, improvedFighter, ...point) {
+    console.log(fighter);
+    console.log(improvedFighter);
+    console.log(point);
+    let round = 0;
+    let fighting = true;
+    let pointsCount = point.length;
+    let enemyHealth, currentFighter;
+    while (fighting === true) {
+        let currentPoint = point[round % pointsCount];
+        if (round % 2 === 0) {
+            currentFighter = fighter;
+            console.log(fighter);
+            enemyHealth = fighter.hit(improvedFighter, currentPoint);
+            console.log(improvedFighter);
+        } else {
+            console.log(improvedFighter.name);
+            currentFighter = improvedFighter;
+            enemyHealth = improvedFighter.hit(fighter, currentPoint);
+        }
+        if (enemyHealth <= 0) {
+            console.log(currentFighter.name + " wins at " + round + " round");
+            fighting = false;
+        }
+
+        round++;
+    }
+    return true;
+}
+
+let fighter1 = new Fighter('Captain America', 1, 90);
+let fighter2 = new ImprovedFighter('Iron Man', 2, 70);
+fight(fighter1, fighter2, 25, 13, 45, 23);
